a9d4b7782b068d85fcf29a96be11290f4d926ad2
Pjotr <pjotr.public05@thebird.nl>
2009-10-15 12:40:55 +0200

    Autoloaders

--- lib/bigbio.rb
+++ lib/bigbio.rb
@@ -4,5 +4,8 @@
 
   autoload :FastaReader, 'bigbio/db/fasta'
   autoload :FastaWriter, 'bigbio/db/fasta'
+  autoload :FastaPairedReader, 'bigbio/db/fasta'
+  autoload :FastaPairedWriter, 'bigbio/db/fasta'
+  autoload :BlastClust, 'bigbio/db/blast'
 
 # end
