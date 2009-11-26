99e05d5b44236c5d603422bfde460cd5343f5b44
Pjotr Prins <pjotr.public01@thebird.nl>
2009-11-26 13:42:43 +0100

    Performance tests between Bioruby and Biolib (EMBOSS). It shows Biolib is 30x faster.

--- test/performance/translate_with_biolib.rb
+++ test/performance/translate_with_biolib.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/ruby
 #
 # Performance testing routing for translating a FASTA file into six
-# reading frames using the Bioruby routines.
+# reading frames using the Biolib (EMBOSS) routines.
 #
 # by Pjotr Prins (c) 2009
 # 
@@ -10,31 +10,30 @@
 #
 # Usage: 
 #
-#   ruby -Ipath_to_bioruby translate_with_bioruby.rb --iter 10 nucleotides.fasta
+#   ruby -Ipath_to_biolib translate_with_biolib.rb --iter 10 nucleotides.fasta
 #
 # Example:
 #
-#   time ruby -I~/izip/git/opensource/bioruby/lib/ translate_with_bioruby.rb --iter 10 ../data/fasta/nt.fa > test.out
+#   time ruby -I~/izip/git/opensource/biolib/lib/ translate_with_bioruby.rb --iter 10 ../data/fasta/nt.fa > test.out
 #
 # Renders on my machine:
 #
 #  96 records 5760 times translated!
-#   real    0m6.414s
-#   user    0m5.928s
-#   sys     0m0.384s
+#   real    0m0.290s
+#   user    0m0.252s
+#   sys     0m0.024s
 #
 # with a large file
 #
 #  22929 records 137574 times translated!
-#   real    9m30.952s
-#   user    8m42.877s
-#   sys     0m32.878s
-
+#   real    0m20.306s
+#   user    0m15.997s
+#   sys     0m1.344s
 
 
 $: << '../../lib'
 
-require 'bio'
+require 'biolib/emboss'
 require 'bigbio'
 
 iter=1
@@ -46,13 +45,17 @@ if fn == '--iter'
 end
 
 nt = FastaReader.new(fn)
+trnTable = Biolib::Emboss.ajTrnNewI(1);
 
 nt.each { | rec |
   (0..iter).each do | repeat |
-    seq = Bio::Sequence::NA.new(rec.seq)
+    ajpseq   = Biolib::Emboss.ajSeqNewNameC(rec.seq,"Test sequence")
+
     [-3,-2,-1,1,2,3].each do | frame |
+      ajpseqt  = Biolib::Emboss.ajTrnSeqOrig(trnTable,ajpseq,frame)
+      aa       = Biolib::Emboss.ajSeqGetSeqCopyC(ajpseqt)
       print "> ",rec.id," ",frame.to_s,"\n"
-      print seq.translate(frame),"\n"
+      print aa,"\n"
     end
   end
 }
